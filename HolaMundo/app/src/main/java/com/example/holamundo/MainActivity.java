ada1b93daf7da48d013569148650803138a4da4d
Jose <garcianavarro.jmiguel@gmail.com>
2023-10-09 13:04:44 +0200

    2023-10-09

--- HolaMundo/app/src/main/java/com/example/holamundo/MainActivity.java
+++ HolaMundo/app/src/main/java/com/example/holamundo/MainActivity.java
@@ -7,18 +7,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("Traza","En evento OnCreate");
-//        TextView texto = new TextView (this);
-//        texto.setText("El ping pong me va a sacar de pobre");
-//        setContentView(texto);
+        Log.i("Traza", "En evento Oncreate");
+        TextView texto = new TextView(this);
+        texto.setText("El ping pong me va a sacar de pobre");
+        setContentView(texto);
+        texto.setTextColor(getResources().getColor(R.color.yellow));
+
     }
+
     protected void salir(View view){
         finishActivity(0);
     }
