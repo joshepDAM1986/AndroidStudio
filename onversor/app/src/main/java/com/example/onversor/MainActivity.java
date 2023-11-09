2cdfffc3d13f1e408c2359c3ab8c6b39190ce2ac
Jose <garcianavarro.jmiguel@gmail.com>
2023-11-09 11:57:29 +0100

    2023/11/09

--- onversor/app/src/main/java/com/example/onversor/MainActivity.java
+++ onversor/app/src/main/java/com/example/onversor/MainActivity.java
@@ -7,18 +7,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    public void convertir(View view){
-        EditText editText=(findViewById(R.id.editTextText));
-        String pesosString= editText.getText().toString();
-        double pesos = Double.parseDouble(pesosString);
-        double dolares = pesos*19.4;
-        String dolaresString = String.format("%.2f",dolares);
-        String mensaje="MNX"+ pesosString+"son USD"+dolaresString;
-        Toast.makeText(this, mensaje,Toast.LENGTH_LONG).show();
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
